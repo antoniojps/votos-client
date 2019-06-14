9f070d1958c353f846ed9d346364db30d84a9f36
António Santos <antoniojps@ua.pt>
2019-06-14 18:25:17 +0100

    Re-add Logo (was deleted by accident)

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,4 +1,4 @@
-export { default as Logo } from "./Logo/Logo"
-export { default as Container } from "./Container/Container"
-export { default as Title } from "./Title/Title"
-export { default as Text } from "./Text/Text"
+export { default as Logo } from './Logo/Logo';
+export { default as Container } from './Container/Container';
+export { default as Title } from './Title/Title';
+export { default as Text } from './Text/Text';
